d18050a144ffbb24a745c7c5512ef5d852784683
shaik sohel <sks510805@gmail.com>
2026-07-04 13:12:37 +0530

    Reverted deployment-specific changes

--- dashboard/app.py
+++ dashboard/app.py
@@ -15,7 +15,7 @@ def load_data():
         os.path.dirname(__file__),
         "..",
         "processed_data",
-        "master_sales_data_sample.csv"
+        "master_sales_data.csv"
     )
 
     print("Loading file from:", file_path)
